7794ba6c1eae24d858de0e2eb3f49e795e3ac9c5
Will Hester <willhester@Wills-MacBook-Air.local>
2014-01-14 21:34:39 -0600

    Major overhaul. No mo' LocationService. Meat went to SettingsFragment.

--- src/com/example/tweakbase/SettingsActivity.java
+++ src/com/example/tweakbase/SettingsActivity.java
@@ -1,18 +1,14 @@
 package com.example.tweakbase;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.IBinder;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
-
-import com.example.tweakbase.LocationService.LocalBinder;
 
 
 /**
@@ -25,86 +21,27 @@ import com.example.tweakbase.LocationService.LocalBinder;
  *
  */
 public class SettingsActivity extends Activity {
-	LocationService locationService;
-	boolean mBound = false;
 	
 	final String TAG = "SettingsActivity";
-	public static final String KEY_PREF_TRACK_LOCATION = "pref_trackLocation";
-	
-	// Listener for if a preference is changed
-	OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-  	  public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-  	    if (key.equals(KEY_PREF_TRACK_LOCATION)) {
-  	    	Log.d(TAG, "Track location preference changed");
-  	    }
-  	  }
-  	};
-	
-  	/**
-  	 * Inherited from the Activity class. Called by the Android OS when
-  	 * the activity is opened.
-  	 * 
-  	 *  @param savedInstanceState	a Bundle (Android class that contains
-  	 *  saved file information) that the Android OS automatically passes
-  	 *  when this method is called.
-  	 */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
-        
-        // Load this activity's SharedPreferences and get the saved preferences
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean trackLocation = sharedPref.getBoolean(KEY_PREF_TRACK_LOCATION, true);
-        
-        Log.d(TAG, "trackLocation is " + trackLocation);
-        if (trackLocation) {
-        	Log.d(TAG, "Trying to bind to LocationService...");
-        	bindToLocationService();
-        } else {
-        	// Unbind from the service
-            if (mBound) {
-                unbindService(mConnection);
-                mBound = false;
-                Log.d(TAG, "SettingsActivity unbound from LocationService");
-            }
-        }
+	/**
+	 * Inherited from the Activity class. Called by the Android OS when
+	 * the activity is opened.
+	 * 
+	 *  @param savedInstanceState	a Bundle (Android class that contains
+	 *  saved file information) that the Android OS automatically passes
+	 *  when this method is called.
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Tells sharedPref to respond to our listener
-        sharedPref.registerOnSharedPreferenceChangeListener(listener);
-    }
-    
-    /**
-     * Helper method that binds this activity to the LocationService
-     * activity. This method is called in onCreate.
-     */
-    private void bindToLocationService() {
-    	// Bind to LocationService
-        Intent intent = new Intent(this, LocationService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-    
-    /** Defines callbacks for service binding, passed to bindService() */
-    private ServiceConnection mConnection = new ServiceConnection() {
+		// Display the fragment as the main content.
+		getFragmentManager().beginTransaction()
+		.replace(android.R.id.content, new SettingsFragment())
+		.commit();
 
-        @Override
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocationService instance
-            LocalBinder binder = (LocalBinder) service;
-            locationService = binder.getService();
-            mBound = true;
-            Log.d(TAG, "SettingsActivity bound to LocationService");
-        }
+	}
 
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
-  
+
 }
--- src/com/example/tweakbase/SettingsFragment.java
+++ src/com/example/tweakbase/SettingsFragment.java
@@ -1,9 +1,27 @@
 package com.example.tweakbase;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+	
+	final String TAG = "SettingsFragment";
+	static final String KEY_PREF_TRACK_LOCATION = "pref_trackLocation";
+	boolean trackMyLocation;
+	LocationManager locManager;
+
+	// The minimum time between updates in milliseconds
+	private static final long TIME_BW_UPDATES = 1000 * 10; // 10 seconds
+
+	TBLocationListener locListener;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -11,5 +29,64 @@ public class SettingsFragment extends PreferenceFragment {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
         
+        locListener = new TBLocationListener();
+        
+        // Load this activity's SharedPreferences and get the saved preferences
+        SharedPreferences sharedPref = getPreferenceManager().getSharedPreferences();
+		trackMyLocation = sharedPref.getBoolean(KEY_PREF_TRACK_LOCATION, true);
+		
+		Log.d(TAG, "In onCreate, preference read as: " + trackMyLocation);
+		
+		if (trackMyLocation) {
+			trackLocation();
+		}
+        
     }
+    
+    private void trackLocation() {
+		Log.d(TAG, "Starting to track location");
+		final Handler mHandler = new Handler();
+		Thread locationThread = new Thread(new Runnable(){ public void run(){
+			mHandler.post(new Runnable(){public void run(){
+				locManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+				locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_BW_UPDATES, 0, locListener);
+			}});
+		}});
+		locationThread.start();
+		Log.d(TAG, "Location tracking started");
+	}
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (key.equals(KEY_PREF_TRACK_LOCATION)) {
+			Log.d(TAG, "Location tracking preference changed");
+			trackMyLocation = sharedPreferences.getBoolean(KEY_PREF_TRACK_LOCATION, true);
+			Log.d(TAG, "In onCreate, preference read as: " + trackMyLocation);
+			
+			if (!trackMyLocation) {
+				if (locManager != null) {
+					locManager.removeUpdates(locListener);
+				}
+				Log.d(TAG, "Locaiton tracking stopped");
+			} else {
+				trackLocation();
+			}
+		}
+		
+	}
+    
 }
